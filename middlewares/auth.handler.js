4e162e6ce88ce390443375287dee8a4956023b47
Santiago Diaz <santi2208@gmail.com>
2024-03-14 12:41:11 -0300

    quito funciones no utilizadas en auth.handler

--- middlewares/auth.handler.js
+++ middlewares/auth.handler.js
@@ -1,24 +1,6 @@
 const boom = require('@hapi/boom')
 const { config } = require('../config/config');
 
-function checkApiKey(req, res, next) {
-    const apiKey = req.headers['api'];
-    if (apiKey === config.apiKey) {
-        next();
-    } else {
-        next(boom.unauthorized());
-    }
-}
-
-function checkAdminRole(req, res, next) {
-    const user = req.user;
-    if (user.role === 'admin') {
-        next();
-    } else {
-        next(boom.unauthorized());
-    }
-}
-
 function checkRoles(...roles) {
     return (req, res, next) => {
         const user = req.user;
@@ -32,4 +14,4 @@ function checkRoles(...roles) {
     }
 }
 
-module.exports = { checkApiKey, checkAdminRole, checkRoles }
+module.exports = { checkRoles }
