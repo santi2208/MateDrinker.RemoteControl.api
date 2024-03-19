9b181a9f6ddcaa3dd454be155ed3415f9756a4dc
Santiago Diaz <santi2208@gmail.com>
2024-03-19 11:58:25 -0300

    Cambio nombre al parametro de consulta de commands by key

--- schemas/command.schema.js
+++ schemas/command.schema.js
@@ -1,12 +1,11 @@
 const Joi = require('joi');
-const key = Joi.string().min(3).max(15);
-const user_id = Joi.string().min(3).max(15);
+const user_id = Joi.string().max(15);
 const command = Joi.string().min(3).max(15);
 const order = Joi.number().integer();
 
 
 const queryCommandsSchema = Joi.object({
-    user_id: key.required()
+    user_id: user_id.required()
 
 });
 
