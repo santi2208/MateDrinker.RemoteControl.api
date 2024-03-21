45ee844bd375387924fa0c3638a393aab8d55b15
Santiago Diaz <santi2208@gmail.com>
2024-03-21 11:49:08 -0300

    Refactor users.router.js: Muevo callback a funciones no anonimas

--- routes/users.router.js
+++ routes/users.router.js
@@ -9,77 +9,82 @@ const passport = require('passport');
 const router = express.Router();
 const service = new UserService();
 
-router.get('/', 
-passport.authenticate('jwt', {session:false}),
-checkRoles('admin'),
-validatorHandler(queryUserSchema, 'query'),
-async (req, res, next) => {
+router.get('/',
+  passport.authenticate('jwt', { session: false }),
+  checkRoles('admin'),
+  validatorHandler(queryUserSchema, 'query'),
+  getUsers);
+
+router.get('/:id',
+  passport.authenticate('jwt', { session: false }),
+  checkRoles('admin'),
+  validatorHandler(getUserSchema, 'params'),
+  getUserById);
+
+router.post('/',
+  validatorHandler(createUserSchema, 'body'),
+  createUser);
+
+router.patch('/:id',
+  validatorHandler(getUserSchema, 'params'),
+  validatorHandler(updateUserSchema, 'body'),
+  updateUser);
+
+router.delete('/:id',
+  passport.authenticate('jwt', { session: false }),
+  checkRoles('admin'),
+  validatorHandler(getUserSchema, 'params'),
+  deleteUser);
+
+async function getUsers(req, res, next) {
   try {
-    
     const users = await service.find(req.query);
     res.json(users);
   } catch (error) {
     next(error);
   }
-});
+};
 
-router.get('/:id',
-  passport.authenticate('jwt', {session:false}),
-  checkRoles('admin'),
-  validatorHandler(getUserSchema, 'params'),
-  async (req, res, next) => {
-    try {
-      const { id } = req.params;
-      const user = await service.findOne(id);
-      res.json(user);
-    } catch (error) {
-      next(error);
-    }
+async function getUserById(req, res, next) {
+  try {
+    const { id } = req.params;
+    const user = await service.findOne(id);
+    res.json(user);
+  } catch (error) {
+    next(error);
   }
-);
+}
 
-router.post('/',
-  validatorHandler(createUserSchema, 'body'),
-  async (req, res, next) => {
-    try {
-      const body = req.body;
-      const newUser = await service.create(body);
-      res.status(201).json(newUser);
-    } catch (error) {
-      next(error);
-    }
+async function createUser(req, res, next) {
+  try {
+    const body = req.body;
+    const newUser = await service.create(body);
+    res.status(201).json(newUser);
+  } catch (error) {
+    next(error);
   }
-);
+}
 
-router.patch('/:id',
-  validatorHandler(getUserSchema, 'params'),
-  validatorHandler(updateUserSchema, 'body'),
-  async (req, res, next) => {
-    try {
-      const { id } = req.params;
-      const body = req.body;
-      const updatedUser = await service.update(id, body);
-      res.status(200).json(updatedUser);
-    } catch (error) {
-      next(error);
-    }
+async function updateUser(req, res, next) {
+  try {
+    const { id } = req.params;
+    const body = req.body;
+    const updatedUser = await service.update(id, body);
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    next(error);
   }
-);
+}
 
-router.delete('/:id',
-  passport.authenticate('jwt', {session:false}),
-  checkRoles('admin'),
-  validatorHandler(getUserSchema, 'params'),
-  async (req, res, next) => {
-    try {
-      const { id } = req.params;
-      await service.delete(id);
-      res.status(204).json({id});
-    } catch (error) {
-      next(error);
-    }
+async function deleteUser(req, res, next) {
+  try {
+    const { id } = req.params;
+    await service.delete(id);
+    res.status(204).json({ id });
+  } catch (error) {
+    next(error);
   }
-);
+}
 
 module.exports = router;
 
