4e71003aa3d33535b753110c3d34d9eec6448561
Santiago Diaz <santi2208@gmail.com>
2024-03-23 00:19:03 -0300

    Agrego un index en la raiz para que lo pueda tomar cyclic

---
+++ index.js
@@ -0,0 +1,20 @@
+const commandsApp = require("./src/api/commands/index-commands")
+const mainApp = require("./src/api/auth-user/index")
+
+const { config } = require('./config/config')
+const { swaggerDocs: v1SwaggerDocs } = require("./utils/swagger/swagger")
+const COMMANDS_PORT = config.apis.commands.port;
+const MAIN_PORT = config.apis.main.port;
+
+mainApp.listen(MAIN_PORT, () => {
+  console.log(`Servidor escuchando en el puerto ${MAIN_PORT}`);
+  const apis = config.swagger.authUser.apis;
+  v1SwaggerDocs(mainApp, MAIN_PORT, apis);
+});
+
+commandsApp.listen(COMMANDS_PORT, () => {
+    console.log(`Servidor escuchando en el puerto ${COMMANDS_PORT}`);
+    const apis = config.swagger.commands.apis;
+    v1SwaggerDocs(commandsApp, COMMANDS_PORT, apis);
+});
+
